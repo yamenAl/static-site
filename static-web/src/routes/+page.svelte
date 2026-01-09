5de5bb2fac27ee8ec82e1a9ee6ea835ad19e53c9
Yamen <62349907+yamenAl@users.noreply.github.com>
2026-01-09 06:04:04 +0100

    adding data and working with supabase #2

--- static-web/src/routes/+page.svelte
+++ static-web/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  let { data } = $props();
+</script>
+
+<h1>hello</h1>
+
+<ul>
+  {#each data.articles as article}
+    <li>{article.titel}</li>
+    <li>{article.body}</li>
+  {/each}
+</ul>
